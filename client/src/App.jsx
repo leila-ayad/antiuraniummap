a9e9d7104cdac268ff88ff188c834bc12ad559e7
Leila <leila.alhemali@gmail.com>
2024-09-08 09:41:16 -0600

    setting up supabase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,54 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+
 import "./App.css";
+import supabase from "../config/supabaseClient";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [fetchError, setFetchError] = useState(null);
+  const [stories, setStories] = useState(null);
+
+  useEffect(() => {
+    const fetchStories = async () => {
+      const { data, error } = await supabase.from("stories").select();
+      if (error) {
+        setFetchError("Error:", error);
+        setStories(null);
+      }
+      if (data) {
+        setStories(data);
+
+        setFetchError(null);
+      }
+    };
+
+    fetchStories();
+  }, []);
 
   return (
     <>
       <div id="map"></div>
-
+      {stories && (
+        <div>
+          {stories.map((story) => (
+            <p key={story.id}>{story.story_body}</p>
+          ))}
+        </div>
+      )}
       <div id="form-container">
         <form>
-          <label for="location">Location:</label>
+          <label>Location:</label>
           <br />
           <input type="text" id="location" name="location" />
           <br />
           <br />
 
-          <label for="story">Story:</label>
+          <label>Story:</label>
           <br />
           <textarea id="story" name="story" rows="4" cols="50"></textarea>
           <br />
           <br />
 
-          <label for="photo">Photo:</label>
+          <label>Photo:</label>
           <br />
           <input type="file" id="photo" name="photo" />
           <br />
